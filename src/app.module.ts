98ece20fba8e2f8f22171607510c29272c829fec
Yesbol (defaul7) Sariyev <defaul7btw@gmail.com>
2026-03-07 11:06:37 +0500

    feat: integrate users and auth modules, and add CLI support with nest-commander

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+import { CommandRunnerModule } from 'nest-commander';
 
 @Module({
   imports: [
@@ -12,6 +16,9 @@ import { join } from 'path';
       isGlobal: true,
     }),
     PrismaModule,
+    UsersModule,
+    AuthModule,
+    CommandRunnerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
